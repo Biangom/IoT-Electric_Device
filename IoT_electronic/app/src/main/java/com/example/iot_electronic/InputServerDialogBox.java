e7b03859d5b61fa2fdc30336482002c9cae2a2fc
Biangom <kss7858@naver.com>
2018-08-20 20:19:19 +0900

    client 생성 및 다이얼로그 완성

--- IoT_electronic/app/src/main/java/com/example/iot_electronic/InputServerDialogBox.java
+++ IoT_electronic/app/src/main/java/com/example/iot_electronic/InputServerDialogBox.java
@@ -3,42 +3,48 @@ package com.example.iot_electronic;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class InputServerDialogBox extends DialogFragment {
-    @BindView(R.id.nameInputText)
     EditText nameInputText;
-    @BindView(R.id.portInputText)
     EditText portInputText;
-    @BindView(R.id.ipInputText)
     EditText ipInputText;
-    @BindView(R.id.connectTryBtn)
+
     Button connectTryBtn;
 
-    // 이 Dialog가 완료 될떄 하는 행위 정의, 이 dialog는 BtDevice 객체를 넘겨주게 된다.
-//    public interface OnCompleteListener{
-//        void onInputedData(BtDevice temp);
-//    }
-//    private OnCompleteListener mCallback;
+    private Fragment fragment;
 
-//    @Override
-//    public void onAttach(Activity activity) {
-//        super.onAttach(activity);
-//        try {
-//            mCallback = (OnCompleteListener) activity;
-//        }
-//        catch (ClassCastException e) {
-//            Log.d("DialogFragmentExample", "Activity doesn't implement the OnCompleteListener interface");
-//        }
-//    }
+    @OnClick(R.id.connectTryBtn)
+    public void onViewClicked() {
+    }
+
+    // 이 Dialog가 완료 될떄 하는 행위 정의.
+    // 이 dialog는 temp객체를 넘겨주게 된다.
+    // MainActivity에서 overriding 해서 데이터를 받을 예정.
+    public interface OnCompleteListener {
+        void onInputedData(Client temp);
+    }
+
+    // 이 다이얼로그를 호출한 액티비티에게
+    // 전달할 데이터
+    private OnCompleteListener mCallback;
+
+    // 자신의 값을 셋팅.
+    public void setDialogResult(OnCompleteListener result) {
+        mCallback = result;
+    }
+
 
     // Dialog가 생성될때 실행되는 함수 정의
     @Override
@@ -49,37 +55,35 @@ public class InputServerDialogBox extends DialogFragment {
         View view = inflater.inflate(R.layout.dialog_input_server, null); // view를 dialog_login.xml로 설정
         builder.setView(view);
 
+        fragment = getActivity().getSupportFragmentManager().findFragmentByTag("dialog");
 
-        // 완료(클릭)를 눌렀을 때 하는 로직 정의
-//        submit.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // 먼저 temp객체를 선언하여, 그 객체에 정보를 담는다.
-//                // 블루투스 디바이스 이름과, 유저 이름을 담을 것이므로 temp의 속성값에 셋팅해준다.
-//                BtDevice temp = new BtDevice();
-//            temp.btName = btName.getText().toString();
-//            temp.userName = userName.getText().toString();
-//            temp.isEncountering = false;
-//            temp.count = 0;
-//            dismiss();
-//                mCallback.onInputedData(temp);
-//            // 전송한다.
-//        }
-//        });
+        connectTryBtn = (Button) view.findViewById(R.id.connectTryBtn) ;
+        nameInputText = (EditText) view.findViewById(R.id.nameInputText);
+        ipInputText = (EditText) view.findViewById(R.id.ipInputText);
+        portInputText = (EditText) view.findViewById(R.id.portInputText);
+
+
+        // '닫기' 기능해 해당하는 버튼을 눌렀을 때
+        //  실행되는 로직 정의
+        connectTryBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("3", "connect Try btn");
+                // 먼저 전달할 client 객체를 선언, 그 정보를 담는다.
+                Client client = new Client(
+                        nameInputText.getText().toString(),
+                        ipInputText.getText().toString(),
+                        portInputText.getText().toString()
+                );
+
+                mCallback.onInputedData(client);
+                DialogFragment dialogFragment = (DialogFragment) fragment;
+                dialogFragment.dismiss();
+            }
+        });
+
         return builder.create();
     }
 
-    @OnClick({R.id.nameInputText, R.id.portInputText, R.id.ipInputText, R.id.connectTryBtn})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.nameInputText:
-                break;
-            case R.id.portInputText:
-                break;
-            case R.id.ipInputText:
-                break;
-            case R.id.connectTryBtn:
-                break;
-        }
-    }
 
 }
