979b2a8a6a6de7bd903e50b420a500d0ba38b97a
Biangom <kss7858@naver.com>
2020-03-22 21:18:02 +0900

    - Android Mqtt 이용하여 실행 성공. - Cloud Mqtt 비밀번호 변경함.

--- empty_project/app/src/main/java/com/example/empty_project/MainActivity.java
+++ empty_project/app/src/main/java/com/example/empty_project/MainActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -23,25 +30,53 @@ public class MainActivity extends AppCompatActivity {
     static private int qos;
     static private String topic;
     static private MemoryPersistence persistence;
+    static private MqttAndroidClient mqttAndroidClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /*
+         *   subscribe 할때 3번째 파라메터에 익명함수 리스너를 달아줄수도있음
+         * */
+        /*try {
+            mqttAndroidClient.subscribe("jmlee!!", 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }*/
 
         resultView = findViewById(R.id.resultView);
+    }
+
+    private DisconnectedBufferOptions getDisconnectedBufferOptions() {
+        DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+        disconnectedBufferOptions.setBufferEnabled(true);
+        disconnectedBufferOptions.setBufferSize(100);
+        disconnectedBufferOptions.setPersistBuffer(true);
+        disconnectedBufferOptions.setDeleteOldestMessages(false);
+        return disconnectedBufferOptions;
+    }
+
+    private MqttConnectOptions getMqttConnectionOption() {
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+
+        mqttConnectOptions.setUserName("uztrfyhg");
+        mqttConnectOptions.setPassword("zN6Oiudz0plw".toCharArray());
+        mqttConnectOptions.setCleanSession(false);
+        //mqttConnectOptions.setAutomaticReconnect(true);
+        //mqttConnectOptions.setWill("aaa", "I am going offlineasd".getBytes(), 1, true);
+        return mqttConnectOptions;
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        try {
-            sampleClient.disconnect();
-            System.out.println("Disconnected");
 
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
     }
 
     // Button.OnclickListener를 implements하므로 onClick() 함수를 오버라이딩.
@@ -52,45 +87,55 @@ public class MainActivity extends AppCompatActivity {
 
         switch (view.getId()) {
             case R.id.sendButton:
-                topic       = "MQTT Examples";
-                qos         = 0;
-                String broker       = "tcp://m16.cloudmqtt.com:14593";
-                String clientId     = "TestName";
-                persistence = new MemoryPersistence();
-
                 try {
-                    sampleClient = new MqttClient(broker, clientId, persistence);
-
-                    MqttConnectOptions connOpts = new MqttConnectOptions();
-                    connOpts.setCleanSession(true);
-                    connOpts.setUserName("uztrfyhg");
-                    connOpts.setPassword("6Zi8XPdWFqed".toCharArray());
-                    System.out.println("Connecting to broker: "+broker);
-                    sampleClient.connect(connOpts);
-                    if(sampleClient.isConnected()) {
-                        System.out.println("###client complete=d");
+                    if(!mqttAndroidClient.isConnected()) {
+                        System.out.println("**** disconnected!!!! ***");
                     }
-                    System.out.println("Connected");
-                    String content;
-                    content = editText.getText().toString();
-                    //resultView.setText("Red");
-                    MqttMessage message = new MqttMessage(content.getBytes());
-
-                    message.setQos(qos);
-                    sampleClient.publish(topic, message);
-                    System.out.println("Publishing message: "+content);
-                    System.out.println("Message published");
-                    //textView1.setBackgroundColor(Color.rgb(255, 0, 0));
-                    break ;
-
-                } catch(MqttException me) {
-                    System.out.println("reason "+me.getReasonCode());
-                    System.out.println("msg "+me.getMessage());
-                    System.out.println("loc "+me.getLocalizedMessage());
-                    System.out.println("cause "+me.getCause());
-                    System.out.println("excep "+me);
-                    me.printStackTrace();
+                    mqttAndroidClient.publish("temper", editText.getText().toString().getBytes(), 0 , false );
+                } catch (MqttException e) {
+                    e.printStackTrace();
                 }
+
+
+//                topic       = "MQTT Examples";
+//                qos         = 0;
+//                String broker       = "tcp://m16.cloudmqtt.com:14593";
+//                String clientId     = "TestName";
+//                persistence = new MemoryPersistence();
+//
+//                try {
+//                    sampleClient = new MqttClient(broker, clientId, persistence);
+//
+//                    MqttConnectOptions connOpts = new MqttConnectOptions();
+//                    connOpts.setCleanSession(true);
+//                    connOpts.setUserName("uztrfyhg");
+//                    connOpts.setPassword("6Zi8XPdWFqed".toCharArray());
+//                    System.out.println("Connecting to broker: "+broker);
+//                    sampleClient.connect(connOpts);
+//                    if(sampleClient.isConnected()) {
+//                        System.out.println("###client complete=d");
+//                    }
+//                    System.out.println("Connected");
+//                    String content;
+//                    content = editText.getText().toString();
+//                    //resultView.setText("Red");
+//                    MqttMessage message = new MqttMessage(content.getBytes());
+//
+//                    message.setQos(qos);
+//                    sampleClient.publish(topic, message);
+//                    System.out.println("Publishing message: "+content);
+//                    System.out.println("Message published");
+//                    //textView1.setBackgroundColor(Color.rgb(255, 0, 0));
+//                    break ;
+//
+//                } catch(MqttException me) {
+//                    System.out.println("reason "+me.getReasonCode());
+//                    System.out.println("msg "+me.getMessage());
+//                    System.out.println("loc "+me.getLocalizedMessage());
+//                    System.out.println("cause "+me.getCause());
+//                    System.out.println("excep "+me);
+//                    me.printStackTrace();
+//                }
 
 
 //            case R.id.buttonGreen :
@@ -101,6 +146,59 @@ public class MainActivity extends AppCompatActivity {
 //                textView1.setText("Blue") ;
 //                textView1.setBackgroundColor(Color.rgb(0, 0, 255));
 //                break ;
+                break;
+
+            case R.id.connectTry:
+                System.out.println("123123123");
+                mqttAndroidClient = new MqttAndroidClient(this,
+                        "tcp://" +
+                                "m16.cloudmqtt.com" +
+                                ":14593" ,
+                        "uztrfyhg");
+                try {
+                    IMqttToken token = mqttAndroidClient.connect(getMqttConnectionOption());    //mqtttoken 이라는것을 만들어 connect option을 달아줌
+                    token.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            mqttAndroidClient.setBufferOpts(getDisconnectedBufferOptions());    //연결에 성공한경우
+                            Log.e("Connect_success", "Success");
+                            try {
+                                mqttAndroidClient.subscribe("jmlee", 0);   //연결에 성공하면 jmlee 라는 토픽으로 subscribe함
+                            } catch (MqttException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {   //연결에 실패한경우
+                            Log.e("connect_fail", "Failure " + exception.toString());
+                        }
+                    });
+
+                } catch (
+                        MqttException e) {
+                    e.printStackTrace();
+                }
+
+                mqttAndroidClient.setCallback(new MqttCallback() {  //클라이언트의 콜백을 처리하는부분
+                    @Override
+                    public void connectionLost(Throwable cause) {
+                    }
+                    @Override
+                    public void messageArrived(String topic, MqttMessage message) throws Exception {    //모든 메시지가 올때 Callback method
+                        if (topic.equals("jmlee")){     //topic 별로 분기처리하여 작업을 수행할수도있음
+                            String msg = new String(message.getPayload());
+                            resultView.setText(msg);
+                            Log.e("arrive message : ", msg);
+                        }
+                    }
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken token) {
+
+                    }
+
+
+                });
+                break;
         }
 
     }
