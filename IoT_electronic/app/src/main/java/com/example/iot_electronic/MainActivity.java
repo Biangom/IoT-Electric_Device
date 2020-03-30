aaaa4eb4951d0a845a7ce12c0e26c38ffdc635ba
Biangom <kss7858@naver.com>
2020-03-30 20:19:19 +0900

    Dialog 완성 및 데이터전달 완료
    Signed-off-by: Biangom <kss7858@naver.com>

--- IoT_electronic/app/src/main/java/com/example/iot_electronic/MainActivity.java
+++ IoT_electronic/app/src/main/java/com/example/iot_electronic/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.iot_electronic;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,12 +18,6 @@ public class MainActivity extends AppCompatActivity {
     Button connectTry;
     @BindView(R.id.signalView)
     Button signalView;
-    @BindView(R.id.resultText)
-    TextView resultText;
-
-    // 연결할 클라이언트
-    private Client connectClient;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,34 +31,18 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.connectTry:
 
-                InputServerDialogBox newFragment = new InputServerDialogBox();
+                DialogFragment newFragment = new InputServerDialogBox();
                 newFragment.show(getSupportFragmentManager(), "dialog"); //"dialog"라는 태그를 갖는 프래그먼트를 보여준다.
-                newFragment.setDialogResult(new InputServerDialogBox.OnCompleteListener() {
-
-
-                    @Override
-                    public void onInputedData(Client client) {
-                        // 서버랑 연결 시도할거임
-                        connectClient = client;
-                    }
-
-                });
-
-                int ret = conenctSerever(connectClient.getClientId(), connectClient.getIp(), connectClient.getPort());
 
 //                AlertDialog.Builder builder = new AlertDialog.Builder();
 //                AlertDialog alertDialog = builder.create();
+//
 //                alertDialog.show();
-
                 break;
             case R.id.signalView:
 
 
                 break;
         }
-    }
-
-    private int conenctSerever(String clientId, String ip, String port) {
-
     }
 }
